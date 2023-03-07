035c23fe8c32857f6e63f600a77b8645c8c8b21c
Koenlaermans <36439934+Koenlaermans@users.noreply.github.com>
2023-03-07 10:45:30 +0100

    update

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
 from haystack.document_stores import InMemoryDocumentStore
-
-document_store = InMemoryDocumentStore(use_bm25=True)
-
+from sentence_transformers import SentenceTransformer, util
+from haystack.document_stores import ElasticsearchDocumentStore
+from haystack.nodes import EmbeddingRetriever
+from haystack.pipelines import ExtractiveQAPipeline
+from haystack.nodes import FARMReader
+from pprint import pprint
+from haystack.utils import print_answers
 from haystack.utils import fetch_archive_from_http
+from haystack.nodes import BM25Retriever
+import os
+from haystack.pipelines import Pipeline
+
+from haystack.pipelines.standard_pipelines import TextIndexingPipeline
+
+
+document_store = ElasticsearchDocumentStore(
+    similarity="dot_product",
+    embedding_dim=768
+)
 
 doc_dir = "corpus\content\manuals_dump"
-
-# fetch_archive_from_http(
-#     url="https://s3.eu-central-1.amazonaws.com/deepset.ai-farm-qa/datasets/documents/wiki_gameofthrones_txt1.zip",
-#     output_dir=doc_dir
-# )
-
-import os
-from haystack.pipelines.standard_pipelines import TextIndexingPipeline
 
 files_to_index = [doc_dir + "/" + f for f in os.listdir(doc_dir)]
 indexing_pipeline = TextIndexingPipeline(document_store)
 indexing_pipeline.run_batch(file_paths=files_to_index)
 
-from haystack.nodes import BM25Retriever
+retriever = EmbeddingRetriever(
+    document_store=document_store,
+    embedding_model="sentence-transformers/multi-qa-mpnet-base-dot-v1",
+    model_format="sentence_transformers"
+)
 
-retriever = BM25Retriever(document_store=document_store)
-
-from haystack.nodes import FARMReader
+document_store.update_embeddings(retriever)
 
 reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-
-from haystack.pipelines import ExtractiveQAPipeline
 
 pipe = ExtractiveQAPipeline(reader, retriever)
 
 prediction = pipe.run(
     query="How do I change the radio frequencies?",
     params={
-        "Retriever": {"top_k": 10},
-        "Reader": {"top_k": 5}
+        "Retriever": {"top_k": 20},
+        "Reader": {"top_k": 20}
     }
 )
-
-from pprint import pprint
-
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 pprint(prediction)
-
-from haystack.utils import print_answers
-
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+print_answers(
+    prediction,
+    details="minimum" ## Choose from `minimum`, `medium`, and `all`
+)
+print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 print_answers(
     prediction,
     details="all" ## Choose from `minimum`, `medium`, and `all`
